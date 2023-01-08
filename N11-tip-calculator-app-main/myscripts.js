53b6716a7f55f3ccb224f8474f8429c0840c38cf
AlexSal1602 <asalbishvili@hotmail.com>
2023-01-08 23:07:38 +0400

    Tip calculator, html done, css done, javascript done, responsive not done

--- N11-tip-calculator-app-main/myscripts.js
+++ N11-tip-calculator-app-main/myscripts.js
@@ -1,81 +1,120 @@
 "use strict";
 
-/* const button = document.querySelectorAll("button")[0];
 
-button.addEventListener("click", (ev) => alert("button clicked")) */
+//number of bill input dom
+let billInput = document.getElementById("bill-input");
 
-
-
-/* alert(`rames mainc dawers${billInputValue}`); */
-
-/* const tipCalculate = () => {
-    let billInputValue = parseInt(document.querySelectorAll("input")[0].value);
-let peopleInputValue = parseInt(document.querySelectorAll("input")[2].value);
-
-const buttonList = document.querySelectorAll("button");
-  buttonList.forEach(function(i){
-    i.addEventListener("click", function(e){
-      let buttonValue = e.target.value;
-      return buttonValue;
-    })
-  });
-
-    let tipAmount = billInputValue * buttonValue/100;
-    console.log (tipAmount);
-}
-
-tipCalculate();
- */
-
-
-
-
-
-const billInput = document.getElementById("bill-input");
-billInput.addEventListener("input", tipCalculate);
-
-
+//number of people input dom
 const peopleInput = document.getElementById("people-input");
-peopleInput.addEventListener("input", tipCalculate);
+
+const customTip = document.getElementById("custom");
+
 
 const tipAmountField = document.querySelector('.tip-amount h1');
 const totalField = document.querySelector('.total h1');
 
 const selectedTip = document.querySelectorAll('input[name="tipButton"]');
 
-const selectedTipValue = selectedTip.forEach(element => {
-    element.addEventListener('click', (e) => e.target.value)
-  
+
+
+// '''''''''''''''''''''''''''''''''''''''''''''''''''''' Reset Button ''''''''''''''''''''''''''''''''''''''''''''''''''''''
+const resetButton = document.querySelector('button[name="resetButton"]');
+
+resetButton.addEventListener('click', reset);
+
+function reset(){
+  customTip.value = "";
+  billInput.value = "";
+  peopleInput.value = "1";
+  selectedTip.forEach((tip)=> tip.checked = false);
+  tipAmountField.innerHTML = '$0.00';
+  totalField.innerHTML = '$0.00';
+  resetButton.style.backgroundColor = "#0D686D";
+}
+
+
+
+billInput.addEventListener("input", tipCalculate);
+peopleInput.addEventListener("input", tipCalculate);
+
+
+selectedTip.forEach(selectedTipButton => {
+  selectedTipButton.addEventListener("click", () => customTip.value = "" );  
+  selectedTipButton.addEventListener('click', tipCalculate);  
 });
 
 
 
 
-/* selectedTip.addEventListener('click', () => alert("ramee"));
- */
+customTip.addEventListener("input", ()=> {
+  selectedTip.forEach((tip)=> tip.checked = false)
+});
 
+
+customTip.addEventListener("input", tipCalculate);
+
+
+
+
+// main function which goes to almost all events
 function tipCalculate() {
   const bill = parseInt(billInput.value);
-  const numOfPeople = parseInt(peopleInput.value);
   let tipAmount = bill || '0.00';
-  let total = 0;
-
-  tipAmountField.innerHTML= `$${tipAmount}`;
-  console.log(selectedTipValue);
-
-
+  const radioButtonValue = getRadioValue();
   
-}
+  if (!customTip.value){
+    const tipAmountFieldValue = Math.floor(tipAmount) * Math.floor(radioButtonValue) / 100 / Math.floor(peopleInput.value);
+    tipAmountField.innerHTML= "$"+ tipAmountFieldValue.toFixed(2);
+    const totalFieldValue = (Math.floor(tipAmount) + Math.floor(tipAmount) * Math.floor(radioButtonValue) / 100) / Math.floor(peopleInput.value);
+    totalField.innerHTML = "$"+ totalFieldValue.toFixed(2);
+  } else {
+    const tipAmountFieldValue = Math.floor(tipAmount) * Math.floor(customTip.value) / 100 / Math.floor(peopleInput.value);
+    tipAmountField.innerHTML= "$"+ tipAmountFieldValue.toFixed(2);
+    const totalFieldValue = (Math.floor(tipAmount) + Math.floor(tipAmount) * Math.floor(customTip.value) / 100) / Math.floor(peopleInput.value);
+    totalField.innerHTML = "$"+ totalFieldValue.toFixed(2);
+  };
+  
+
+  //change reset button color
+  
+  resetButton.style.backgroundColor = "#26C2AE";
+
+};
+
+function getRadioValue(){
+  let selectedValue = null;
+  
+  selectedTip.forEach((tip) => {
+    if (tip.checked){
+      selectedValue = tip.value;
+    } 
+    
+  });
+  return selectedValue;
+};
 
 
 
+//Prevent leteters in inputs
+preventLetters(customTip);
+preventLetters(billInput);
 
-
-
-/* function getValue(e) {
-  log.textContent = e.target.value;
-  return e.target.value;
-} 
-
-const log = document.getElementById('values');
-*/
+function preventLetters(target){
+  target.addEventListener("keydown", function(event) {
+    // Allow: backspace, delete, tab, escape, and enter
+    if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
+         // Allow: Ctrl+A
+        (event.keyCode == 65 && event.ctrlKey === true) || 
+         // Allow: home, end, left, right
+        (event.keyCode >= 35 && event.keyCode <= 39)) {
+          // let it happen, don't do anything
+          return;
+    }
+    else {
+      // Ensure that it is a number and stop the keypress
+      if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+        event.preventDefault(); 
+      }   
+    }
+  });
+};
